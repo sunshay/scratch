5dad80973969f9629ac6a2473411cd259dfcedf2
sunshay <sunshayina@outlook.com>
2022-05-07 09:07:45 -0700

    convert a integer to string

---
+++ type.py
@@ -0,0 +1,10 @@
+#!usr/bin/python
+
+a = 'fred'
+b = str(40)
+weather = "\tIt's kind of sunny,\nhope we have a good day!"
+
+print('I am ' + a + " my age is "+ b) #b is a integer but we add str to enforce print 
+print(weather)
+
+
