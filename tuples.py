3a0729dc29853ec89ba31c56dc2980bae4a4dd60
sunshay <sunshayina@outlook.com>
2022-05-26 04:11:31 -0700

    create tuples

---
+++ tuples.py
@@ -0,0 +1,7 @@
+#Tuple is ordered and unchangeable
+#examples includes...
+myTuple = ("David", "Sun", "Brian")
+print(myTuple)
+
+print (myTuple[1]) #this returns element with position one.
+#where element with position one is Sun.
