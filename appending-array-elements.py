5018e421b0c200ccad79a734b6550ef25b1b1efe
sunshay <sunshayina@outlook.com>
2022-05-26 02:24:40 -0700

    Create appending-array-elements.py

---
+++ appending-array-elements.py
@@ -0,0 +1,14 @@
+"""
+you can use the append method to add more element into an array....
+"""
+name = ['DAVID', 'BRIAN','SUNSHAYINA']
+
+#Array can hold more values at a time....
+#print name[0] #this is to print array of names....
+#this array will fetch user name David.
+
+
+name.append('OGUTI')
+
+for x in name:
+    print x
