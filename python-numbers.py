78ed0f1df1efe8e709f4816207682ef7789275f8
sunshay <sunshayina@outlook.com>
2022-05-26 21:59:50 -0700

    new commit about numbers

---
+++ python-numbers.py
@@ -0,0 +1,9 @@
+#there are three different numbers in python, eg,
+#int -> this deals with counting numbers.
+#float -> this deals with numbers with decimal place values.
+#complex numbers -> this deals with a combination of numbers and letters. it is a complete mixture.
+
+a = 4 #int
+b = 34.565 # this is flaoting value
+name = "Sun" # this is a complex value.
+print "my name is " + name + " and am " + str(a) + " years old. "
---
+++ python-strings.py
@@ -0,0 +1,13 @@
+#python strings are surrounded by either single or double quotes.
+name = "SUN"
+print name[0]  #-> this will print the first letter on my name . eg, S
+
+print name[1:2] #-> this will print out the element between S and N, and that is U.
+
+x = " David"
+print (x.strip()) #-> this will remove the white space on the first letter from our element.
+
+#converting text to lower cases...
+print (x.lower())
+#converting text to upper cases...
+print (x.upper())
