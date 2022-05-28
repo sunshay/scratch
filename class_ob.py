8fc3a14f3974cf36658e382d13add78e11e245e3
sunshay <sunshayina@outlook.com>
2022-05-27 22:45:10 -0700

    nonsense

---
+++ class_ob.py
@@ -0,0 +1,8 @@
+"""
+Python is an Object Oriented Programming language that everything is done under objects and methods in a class.
+"""
+class MyClass:
+    age = 5
+
+p1 = MyClass()
+print p1.age
