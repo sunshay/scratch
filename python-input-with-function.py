3e82275abbb6af5a402760085f2e599707c50e75
sunshay <sunshayina@outlook.com>
2022-05-31 09:36:52 -0700

    function

---
+++ python-input-with-function.py
@@ -0,0 +1,8 @@
+"""
+Here we are going to make a python input call inside a python function...
+"""
+def my_input():
+    name = raw_input("Enter your name\n")
+    age = raw_input("Enter your age \n")
+    print "My name is " +name +" & age " +  age
+my_input() #this is python call.
