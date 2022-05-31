5df419a299d12bc533e493cc8b211b7566bbe8d9
sunshay <sunshayina@outlook.com>
2022-05-31 09:36:32 -0700

    operator

---
+++ operators.py
@@ -0,0 +1,27 @@
+#operators gives us chance to deal with different operands .
+"""
+we have different types of operators, etc,
+assignment operators,
+increment and decrement operators,
+logic operators,
+arithmetic operators
+"""
+#================> arithmetic operators ==============
+#this deals with additions , subtraction, division, multiplication, modules of a number..
+a = 4
+b = 5
+c = a + b
+print c #this is addion..
+c = a-b
+print c #sub
+c = a/b
+print # division.
+c = a * b
+print c # multiplication..
+
+
+print "=====================working with relational operators..."
+if(a > b):
+    print "A is greater than B"
+else:
+    print "B is greater than A"
