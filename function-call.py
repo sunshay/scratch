d51a4fe311c06c149a964b3121e92481bf1bd1d9
sunshay <sunshayina@outlook.com>
2022-06-02 22:21:59 -0700

    simple function

---
+++ function-call.py
@@ -0,0 +1,10 @@
+#functions helps us to make a simple python program.
+
+#to write a function program, consider giving function declaration and function call..
+
+def Sun_programmer():
+    name = "Sunshayina"
+    age = 23
+    print ("my name is " + name + " and my age is "+ str(age))
+    #where str converts our integer into python string.
+Sun_programmer()
