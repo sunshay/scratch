e22a133f4042acbb2e77429666f82c32873c1dd7
sunshay <sunshayina@outlook.com>
2022-06-03 22:00:48 -0700

    test condition

---
+++ conditions.py
@@ -0,0 +1,11 @@
+#conditional statements with python...
+#here we are learning about how to use python if statements with operators having operants.
+#eg.
+a = 5
+b = 8
+if(a > b):
+    print "A is greater than B"
+    #this statement improves that A is greater than B in condition A is greater.
+else:
+    print"B is greater than A"
+    #this is not a condition, it is a statement, printed out only when A is smaller than B.
