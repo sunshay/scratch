2ac469191a0e0283f835b48ae3a69aa5aac7162c
sunshay <sunshayina@outlook.com>
2022-06-05 18:27:20 -0700

    loops

---
+++ more-variables.py
@@ -0,0 +1,8 @@
+#In variable declaration, you have to respect special characters,
+#things like \, /,@,const, def, etc. those are python special characters that shouldn't be
+#added into your python codes.
+
+first_name = "David"
+last_name = "Brian"
+
+print ("My name is "+ first_name+' '+last_name)
---
+++ more-while-loops.py
@@ -0,0 +1,9 @@
+#lets do more loops in python...
+
+#========> lets make a python program to check for a number, if a number is greater than 5, stop...
+a = 2
+while(a < 10):
+    print a
+    if(a == 5):
+        break
+    a += 1
