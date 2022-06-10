7c81cf00efa9682033c0535d1039a7d00b583d65
sunshay <sunshayina@outlook.com>
2022-06-09 21:52:30 -0700

    dictionarry

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+#A dictionary is a collection which is unordered, changeable and indexed.
+# In Python dictionaries are written with curly brackets,
+# and they have keys and values.
+myDict = {
+  "Mango": "green",
+  "banana": "yellow",
+  "cherry": "red"
+}
+print(myDict)
