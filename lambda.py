db13722c5221dfa502b1473ffb2039a0489fa291
sunshay <sunshayina@outlook.com>
2022-06-09 21:51:44 -0700

    lambda

---
+++ lambda.py
@@ -0,0 +1,7 @@
+"""
+Lambda can take any python argument.. but it returns only one result.
+"""
+a = lambda y:  y + 10
+
+print a
+#this will give an output as 0x000000000002D9E5F8 lambdad function...
