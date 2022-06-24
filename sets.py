33350bae6fe2fa4f81106456db645a50800a8e64
sunshay <sunshayina@outlook.com>
2022-06-24 06:01:11 -0700

    sets

--- sets.py
+++ sets.py
@@ -1,4 +1,7 @@
 #A set is a collection which is unordered and unindexed.
 
-mySet = {"David", "Brian", "Sun"}
-print(mySet.remove('Sun')) #this is how to remove a set in python. 
+#mySet = {"David", "Brian", "Sun"}
+#print(mySet.remove('Sun')) #this is how to remove a set in python. 
+myset = {"books,pen,shirt"}
+print(type(myset))
+print(myset)
