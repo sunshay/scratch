1b1501dab56aa32b133df343c608df8190fc1ec3
sunshay <sunshayina@outlook.com>
2022-06-29 22:49:29 -0700

    numpy

---
+++ t.py
@@ -0,0 +1,15 @@
+from cmath import sqrt
+from numpy import square
+
+
+# a = 1
+
+# while a < 5:
+#         print(a)
+#         a += 1
+
+a = [1,2,3,4,5,6]
+
+for i in a :
+    print(sqrt(i))
+    print(square)
