6571d15ab31d819abca03faf30a7f8b430f82480
sunshay <sunshayina@outlook.com>
2022-07-07 07:25:52 -0700

    increment

---
+++ while-loop.py
@@ -0,0 +1,8 @@
+#while loop statement.
+#this works with increment and decrement statements.
+a = 1
+while(a < 10):
+    print a
+    a += 1
+
+    #this will give you the while loop statement showing the end of a loop . 
