90422d0732be1bffb599c685db5c9fd5e6855789
sunshay <sunshayina@outlook.com>
2022-07-08 22:18:56 -0700

    arrays

---
+++ more-arrays.py
@@ -0,0 +1,9 @@
+"""
+arrays can be used to fetch a single value from the lists of values and the first value in a
+array is considered to be indexed value, remember array starts from zero(0)
+"""
+name = ['DAVID', 'BRIAN','SUNSHAYINA']
+
+#Array can hold more values at a time....
+print name[0] #this is to print array of names....
+#this array will fetch user name David.
