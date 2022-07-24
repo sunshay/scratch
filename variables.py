b1fd9e368ad2f2a85baaf7dbcac1dcce4ac15410
sunshay <sunshayina@outlook.com>
2022-07-23 23:53:42 -0700

    variable

---
+++ variables.py
@@ -0,0 +1,8 @@
+# to write a variable in python
+#you have to consider letters or strins(characters). eg, a,b,c , or firstname.
+#on variable declarations....
+x = 3
+print "the value for x is "+ str(x) #this converts the value of x to str..
+#this is because python can not print integer value with a string the same time in one line.
+#you have to convert it to string so that it can interpret.
+
