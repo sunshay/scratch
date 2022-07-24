c034a692da88b352644905319e41988037648789
sunshay <sunshayina@outlook.com>
2022-07-23 23:53:31 -0700

    eko

---
+++ comments.py
@@ -0,0 +1,10 @@
+#this is a single line comment,
+#you can always write it in different lines by adding on hash at the beginnig of each line.
+
+
+"""
+this is also another python comment.
+you can always use it in more than just one line
+just like the way you see me do it.
+"""
+print "This is my print out statement, it is not a comment, so it will print out. "
